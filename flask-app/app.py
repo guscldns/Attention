124bc65cdd99ee182474b07785b7130ca97adbcc
guscldns <130722839+guscldns@users.noreply.github.com>
2023-09-30 21:56:40 +0900

    Add files via upload

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,10 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+from google.cloud import vision
+import os
 
 app = Flask(__name__)
 
+# Set the Google Cloud credentials environment variable
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\guscl\OneDrive\문서\Github\api-project-397750607032-5ddc025931cd.json"
+
 @app.route("/")
 def index():
-    return render_template("highlight.html")
+    return render_template("highlight_input.html")
+
+@app.route("/highlight", methods=["POST"])
+def upload():
+    if "file" not in request.files:
+        return "No file part"
+    
+    file = request.files["file"]
+    
+    if file.filename == "":
+        return "No selected file"
+    
+    if file:
+        client = vision.ImageAnnotatorClient()
+        content = file.read()
+        image = vision.Image(content=content)
+
+        response = client.text_detection(image=image)
+        texts = response.text_annotations
+
+        if texts:
+            detected_text = texts[0].description
+            text = detected_text
+            id="textInputForm"
+            
+            # "highlight.html" 템플릿을 렌더링하고 텍스트를 전달하여 표시
+            return render_template("highlight.html", text=text)
+
+    return "OCR failed"
 
 if __name__ == "__main__":
+    # 애플리케이션 실행
     app.run(debug=True)
--- flask-app/static/highlight.js
+++ flask-app/static/highlight.js
@@ -1,30 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const textInputForm = document.getElementById("textInputForm");
     const highlightedText = document.getElementById("highlightedText");
+    const highlightButton = document.getElementById("highlightButton");
     const speedRange = document.getElementById("speedRange");
     const speedValue = document.getElementById("speedValue");
-    const highlightedTextContainer = document.getElementById("highlightedTextContainer");
-    const stopButton = document.getElementById("stopButton");
-
+    
+    let textToHighlight = ""; // 텍스트를 저장할 변수
     let words = []; // 단어 목록
     let index = 0; // 단어 인덱스
     let highlightInterval; // 하이라이트 인터벌
 
-    // 텍스트 입력 폼 제출 이벤트 핸들러
-    textInputForm.addEventListener("submit", function (e) {
-        e.preventDefault();
+    // 버튼 클릭 이벤트 핸들러
+    highlightButton.addEventListener("click", function () {
         if (!highlightInterval) {
-            const text = textInputForm.querySelector("textarea[name='text']").value;
-            words = text.split(" ");
+            textToHighlight = highlightedText.innerText;
+            words = textToHighlight.split(" ");
             index = 0;
             highlightedText.innerHTML = ""; // 하이라이트 초기화
             startHighlight();
+        } else {
+            clearInterval(highlightInterval);
+            highlightInterval = null;
+            highlightButton.textContent = "Start Highlight";
         }
     });
 
     // 슬라이더 변경 이벤트 핸들러
     speedRange.addEventListener("input", function () {
-        const speed = parseInt(speedRange.value);
+        const speed = 101 - parseInt(speedRange.value); // 슬라이더 값을 역으로 계산
         speedValue.textContent = speed + " ms";
         clearInterval(highlightInterval);
         startHighlight();
@@ -32,7 +34,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // 하이라이트 시작 함수
     function startHighlight() {
-        const speed = parseInt(speedRange.value);
+        const speed = 101- parseInt(speedRange.value);
         highlightInterval = setInterval(function () {
             if (index < words.length) {
                 const word = words[index];
@@ -44,13 +46,9 @@ document.addEventListener("DOMContentLoaded", function () {
             } else {
                 clearInterval(highlightInterval);
                 highlightInterval = null;
+                highlightButton.textContent = "Start Highlight";
             }
         }, speed);
+        highlightButton.textContent = "Stop Highlight";
     }
-
-    // 중지 버튼 클릭 이벤트 핸들러
-    stopButton.addEventListener("click", function () {
-        clearInterval(highlightInterval);
-        highlightInterval = null;
-    });
 });
